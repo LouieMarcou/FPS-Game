e2148f23abee3b19399ca536e737d77517aff507
Wildmike-Lou <58005859+Wildmike-Lou@users.noreply.github.com>
2022-04-27 09:00:45 -0500

    added map

---
+++ FPS Game V2/Assets/myStuff/move.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    //public CharacterController controller;
+    //public float speed = 12f;
+
+    //public float mouseSense = 100f;
+
+    //public Transform playerBody;
+
+    //float xRotation = 0f;
+
+    //Vector3 velocity;
+    //public float gravity = -9.81f;
+    //public Transform groundCheck;
+    //public float groundDistance = 0.4f;
+    //public LayerMask groundMask;
+    //bool isGrounded;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    Cursor.lockState = CursorLockMode.Locked;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+    //    if(isGrounded && velocity.y < 0)
+    //    {
+    //        velocity.y = -2f;
+    //    }
+
+    //    float mouseX = Input.GetAxis("Mouse X") * mouseSense * Time.deltaTime;
+    //    float mouseY = Input.GetAxis("Mouse Y") * mouseSense * Time.deltaTime;
+
+    //    float x = Input.GetAxis("Horizontal");
+    //    float z = Input.GetAxis("Vertical");
+
+    //    //Movement
+    //    Vector3 move = transform.right * x + transform.forward * z;
+
+    //    controller.Move(move * speed * Time.deltaTime);
+
+    //    //Camera turn/lock
+    //    xRotation -= mouseY;
+    //    xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+    //    transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    //    playerBody.Rotate(Vector3.up * mouseX);
+
+    //    velocity.y += gravity * Time.deltaTime;
+    //    controller.Move(velocity * Time.deltaTime);
+    //}
+}
